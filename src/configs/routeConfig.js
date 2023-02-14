1e955c81c257d38d387ce36ecf119de0030f9cd5
LeHoaiVu <lehoaivu521997@gmail.com>
2023-02-14 15:30:27 +0700

    update route

--- src/configs/routeConfig.js
+++ src/configs/routeConfig.js
@@ -1,8 +1,8 @@
-export const HOME_PAGE = '/';
-export const MEN_SHOES_PAGE = '/men';
-export const REGISTER_PAGE = '/register';
-export const LOGIN_PAGE = '/login';
-export const MY_SHOP = '/my-shop/';
-export const MY_SHOPING_CART = '/my-shopping-cart/';
-export const CHECK_OUT = '/checkout';
-export const ORDER_PAGE = '/my-order';
+export const HOME_PAGE = 'SneakerEcomerce_FE/';
+export const MEN_SHOES_PAGE = 'SneakerEcomerce_FE/men';
+export const REGISTER_PAGE = 'SneakerEcomerce_FE/register';
+export const LOGIN_PAGE = 'SneakerEcomerce_FE/login';
+export const MY_SHOP = 'SneakerEcomerce_FE/my-shop/';
+export const MY_SHOPING_CART = 'SneakerEcomerce_FE/my-shopping-cart/';
+export const CHECK_OUT = 'SneakerEcomerce_FE/checkout';
+export const ORDER_PAGE = 'SneakerEcomerce_FE/my-order';
